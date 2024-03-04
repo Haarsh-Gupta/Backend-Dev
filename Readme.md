799697b823d28687aa64e59b3522e9bfd67f0000
Haarsh-Gupta <harshgupta08022002@gmail.com>
2024-03-04 10:45:44 +0530

    Connect DataBase

--- Readme.md
+++ Readme.md
@@ -7,3 +7,5 @@ Learn backend before the mid sem exams
 ### Database Model link below
 
 -[Model link](https://app.eraser.io/workspace/YtPqZ1VogxGy1jzIDkzj)
+
+password = 3nqzLzQVuS4hydNw
---
+++ src/index.js
@@ -0,0 +1,57 @@
+// require('dotenv').config({path : './env'})
+import dotenv from 'dotenv'
+import connectDB from "./db/index.js";
+
+dotenv.config({path : './env'})
+
+connectDB() ;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+import mongoose from 'mongoose';
+import { DB_NAME } from './constants.js';
+import express from 'express';
+const app = express() ;
+
+(async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+        app.on('error' , (error) => {
+            console.log("Error by clg of app" , error) 
+            throw(error)
+        })
+
+        app.listen(process.env.PORT , () => {
+            console.log(`You app is listening of PORT :- ${process.env.PORT}`)
+        })
+    } catch (error) {
+        console.log("ERROR :" , error)
+        throw error ;   
+    }
+})()
+
+*/
