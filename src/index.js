6e877d9cecbb7a0dc4ee4c093279983c61b048f5
Haarsh-Gupta <harshgupta08022002@gmail.com>
2024-03-06 10:13:36 +0530

    Api response , error and wrapper or middleware

--- src/index.js
+++ src/index.js
@@ -4,9 +4,20 @@ import connectDB from "./db/index.js";
 
 dotenv.config({path : './env'})
 
-connectDB() ;
-
-
+connectDB()
+.then(() => {
+    const port = process.env.PORT || 8000 ;
+    app.listen(port , () => {
+        console.log(`Server is running at port :- ${port}`);
+    })
+})
+.catch((err) => {
+    app.on('error' , (error) => {
+        console.log("Error by clg of app" , error) 
+        throw(error)
+    })
+    console.log("MONGO DB CONNECTION FAILED !!" , err) ;
+})
 
 
 
